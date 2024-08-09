35c1bac68c8282e8efd7fa158a9eefbf23c42ad0
Aditya Kanthe <adityakanthe10@gmail.com>
2024-08-09 21:20:34 +0530

    completed a simple task of assigment(created a card )

--- Assignments/week-5/BusinessCard/src/App.jsx
+++ Assignments/week-5/BusinessCard/src/App.jsx
@@ -1,9 +1,25 @@
 import Card from "./Card"
-import {data} from "./index.js"
-export default function App({data}){
+// import {data} from "./index.js"
+export default function App(){
+
+  // const item = data[0];
+
+const data = [
+    {
+      id: 1,
+      name:"lokeshwar",
+      description: " A student in 100xDevs Cohort2.0 ",
+      interest1: "Ionic",
+      interest2: "Open Source",
+      interest3: "App Dev",
+   },
+  ];
+  
+
   return (
+    
     <div>
-      <Card  description={data.description} interest1={data.interest1} interest2={data.interest2} interset3 = {data.interest3}/>
+      <Card name ={data[0].name} description={data[0].description} interest1={data[0].interest1} interest2={data[0].interest2} interest3 = {data[0].interest3}/>
     </div>
   )
 }
--- Assignments/week-5/BusinessCard/src/Card.jsx
+++ Assignments/week-5/BusinessCard/src/Card.jsx
@@ -1,17 +1,20 @@
-export default function Card({description,interest1,interest2,interest3}){
+import "./Card.css"
 
+export default function Card({name,description,interest1,interest2,interest3}){
+
+    
     return(
-        <>
-        <h1 className = "name">{description} </h1>
-        <p className="description"> A student in 100xDevs Cohort 2.0 </p>
+        <div className="card">
+        <h1 className = "name">{name}</h1>
+        <p className="description"> {description} </p>
     
         <h2>Interests</h2> 
         <p className="interest1">Ionic</p>
         <p className = "interest2">Open Source</p>
         <p className = "interest3">App Dev</p>
     
-        <button className = "Linkedin">Linkedin</button>
-        <button className="Twitter">Twitter</button>
-        </>
+        <button className= "linkedin">Linkedin</button>
+        <button className="twitter">Twitter</button>
+        </div>
     )
 }
