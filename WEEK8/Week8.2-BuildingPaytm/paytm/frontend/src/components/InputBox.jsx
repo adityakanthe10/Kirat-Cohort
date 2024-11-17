c5c55d1a1ae4a1701b998ca1a3cc913785aa9382
adityakanthe10 <adityakanthe10@gmail.com>
2024-11-17 10:55:30 +0530

    bootstraped frontend

--- WEEK8/Week8.2-BuildingPaytm/paytm/frontend/src/components/InputBox.jsx
+++ WEEK8/Week8.2-BuildingPaytm/paytm/frontend/src/components/InputBox.jsx
@@ -1,9 +1,13 @@
-
-export function InputBox({label, placeholder, onChange}) {
-    return <div>
-      <div className="text-sm font-medium text-left py-2">
-        {label}
-      </div>
-      <input onChange={onChange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+/* eslint-disable react/prop-types */
+export function InputBox({ label, placeholder, onChange }) {
+  return (
+    <div>
+      <div className="text-sm font-medium text-left py-2">{label}</div>
+      <input
+        placeholder={placeholder}
+        onChange={onChange}
+        className="w-full px-2 py-1 border rounded border-slate-200"
+      />
     </div>
+  );
 }
