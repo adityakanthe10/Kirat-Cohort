097b74f50e6ecc9337c70afcf44ae309f4471c0b
adityakanthe10 <adityakanthe10@gmail.com>
2024-11-17 12:54:37 +0530

    add integration into the signup page

--- WEEK8/Week8.2-BuildingPaytm/paytm/frontend/src/pages/SendMoney.jsx
+++ WEEK8/Week8.2-BuildingPaytm/paytm/frontend/src/pages/SendMoney.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // import { useSearchParams } from "react-router-dom";
 // import axios from "axios";
 // import { useState } from "react";
@@ -9,35 +10,36 @@ export const SendMoney = () => {
   // const [amount, setAmount] = useState(0);
 
   return (
-    <div class="flex justify-center h-screen bg-gray-100">
+    <div className="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
-        <div class="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
-          <div class="flex flex-col space-y-1.5 p-6">
-            <h2 class="text-3xl font-bold text-center">Send Money</h2>
+        <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
+          <div className="flex flex-col space-y-1.5 p-6">
+            <h2 className="text-3xl font-bold text-center">Send Money</h2>
           </div>
-          <div class="p-6">
-            <div class="flex items-center space-x-4">
-              <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span class="text-2xl text-white">A</span>
+          <div className="p-6">
+            <div className="flex items-center space-x-4">
+              <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
+                <span className="text-2xl text-white">A</span>
               </div>
-              <h3 class="text-2xl font-semibold"> Friend's Name</h3>
+              <h3 className="text-2xl font-semibold"> Friend's Name</h3>
             </div>
-            <div class="space-y-4">
-              <div class="space-y-2">
+            <div className="space-y-4">
+              <div className="space-y-2">
                 <label
-                  class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  // eslint-disable-next-line react/no-unknown-property
                   for="amount"
                 >
                   Amount (in Rs)
                 </label>
                 <input
                   type="number"
-                  class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
+                  className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
                 />
               </div>
-              <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+              <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                 Initiate Transfer
               </button>
             </div>
--- WEEK8/Week8.2-BuildingPaytm/paytm/frontend/src/pages/Signup.jsx
+++ WEEK8/Week8.2-BuildingPaytm/paytm/frontend/src/pages/Signup.jsx
@@ -9,18 +9,57 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
+  const [firstName, setFirstName] = useState(" ");
+  const [username, setusername] = useState(" ");
+  const [lastName, setLastName] = useState(" ");
+  const [password, setPassword] = useState(" ");
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox label={"First Name"} />
-          <InputBox label={"Last Name"} />
-          <InputBox label={"Email"} />
-          <InputBox label={"Password"} />
+          <InputBox
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            placeholder="John"
+            label={"First Name"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            label={"Last Name"}
+            placeholder="Doe"
+          />
+          <InputBox
+            onChange={(e) => {
+              setusername(e.target.value);
+            }}
+            label={"Email"}
+            placeholder="adityakanthe@gmail.com"
+          />
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            label={"Password"}
+            placeholder="12345"
+          />
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button
+              onClick={() => {
+                axios.post("http://localhost:3000/api/v1/user/signup", {
+                  username,
+                  password,
+                  firstName,
+                  lastName,
+                });
+              }}
+              label={"Sign up"}
+            />
           </div>
           <BottomWarning
             label={"Already have an account?"}
